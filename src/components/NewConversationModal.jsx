4f8e482e272bdf60aaae07ca36f061d7948b4df3
mirakov <holturaevm@gmail.com>
2022-12-16 01:04:17 +0500

    work on conversationsModal

--- src/components/NewConversationModal.jsx
+++ src/components/NewConversationModal.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useContacts } from "../context/ContactsProvider";
+import { useConversations } from "../context/conversationsProvider";
 
-const NewConversationModal = () => {
+const NewConversationModal = ({ closeModal }) => {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // createConversation(selectedContactIds);
+    closeModal();
+  }
+
+  function handleCheckboxChange(contactId) {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevSelectedContactIds, contactId];
+      }
+    });
+  }
+
   return (
-    <div>NewConversationModal</div>
-  )
-}
+    <>
+      <Modal.Header closeButton>Create Conversation</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controllId={contact.id} key={contact.id}>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckboxChange(contact.id)}
+              ></Form.Check>
+            </Form.Group>
+          ))}
+          <Button type="submit" style={{ margin: "10px 0" }}>
+            Create
+          </Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
+};
 
-export default NewConversationModal
+export default NewConversationModal;
